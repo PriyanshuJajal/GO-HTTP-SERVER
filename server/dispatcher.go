5853e602ebc69e66d7a46ec11605aa4446e38e3c
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-05-05 10:10:40 +0530

    feat: add concurrent TCP server with worker pool & job queue

---
+++ server/dispatcher.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"fmt"
+	"net"
+)
+
+func Start(port string, numWorkers int) {
+	// Creating the TCP Listener on the given port
+	listener, err := net.Listen("tcp" , port);
+
+	if err != nil {
+		fmt.Println("Failed to start server: " , err)
+		return
+	}
+
+	// Ensuring that the socket closes when the server shuts down
+	defer listener.Close()
+
+	fmt.Printf("Server successfully listening on port %s...\n", port)
+
+	// Creating the Buffered Channel (The Job Queue)
+	// This holds up to 100 pending connections at a time
+	
+	jobQueue := make(chan net.Conn , 100)
+
+	// These goroutines will just wait in the background for jobs
+	for i := 1; i <= numWorkers; i++ {
+		go worker(i , jobQueue)
+	}
+
+	// The Infinite Accept Loop
+	for {
+		// Blocks until a client connects
+		conn , err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		// Pushing the new connection into the channel queue
+		jobQueue <- conn
+	}
+}
+
+// Dummy worker function for now
+func worker(id int , jobs <- chan net.Conn) {
+	for conn := range jobs {
+		fmt.Printf("Worker %d received a connection!\n" , id)
+		
+		conn.Close()
+	}
+}
+
