4ccd1e9d532b02d1a26cd36e6897c2418fc93d94
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-05-05 19:21:54 +0530

    Refactor main file to make it as simple as possible

--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
 import (
-	"GO-HTTP-SERVER/server"
 	"fmt"
+	"GO-HTTP-SERVER/handler"
+	"GO-HTTP-SERVER/server"
 )
 
 func main() {
 	port := ":8080"
-	numWorkers := 10 // Spawn 10 concurrent threads (goroutines)
+	numWorkers := 10
 
 	router := server.NewRouter()
 
-	// Defining a GET Route
-	router.Handle("GET" , "/api/status" , func(req server.HTTPRequest) server.HTTPResponse {
-		return server.HTTPResponse{
-			StatusCode: 200,
-			ContentType: "application/json",
-			Body: `{"status":"Server is running smoothly!"}`,
-		}
-	})
-
-	// Defining a POST Route
-	router.Handle("POST" , "/api/data" , func(req server.HTTPRequest) server.HTTPResponse {
-		responseBody := fmt.Sprintf(`{"message":"Data received!", "yourData": %s}`, req.Body)
-		
-		return server.HTTPResponse{
-			StatusCode: 201,
-			ContentType: "application/json",
-			Body: responseBody,
-		}
-	})
+	// Routes for GET & POST request
+	router.Handle("GET", "/api/status", handlers.GetStatus)
+	router.Handle("POST", "/api/data", handlers.PostData)
 
 	fmt.Println("=====================================")
 	fmt.Println("   Starting Go Micro HTTP Server     ")
 	fmt.Println("=====================================")
-	fmt.Printf("\nThread Pool Size: %d\n", numWorkers)
+	fmt.Printf("\n Thread Pool Size: %d\n", numWorkers)
 
-	// Call the Start function from dispatcher.go
 	server.Start(port , numWorkers , router)
 }
